47bad253bd2c1e7009b463f673fdccb1ed2e003f
Maxwell Farrell <richard.farrell2@sky.uk>
2017-02-26 23:14:47 +0000

    asdasd

--- src/index.js
+++ src/index.js
@@ -45,9 +45,7 @@ exports.handler = (event, context, callback) => {
         callback(null, { headers, statusCode: 200, body: { description: '' } })
       })
       .catch(error => {
-        logger.error(`Handler: ${error}`)
-
-        callback(null, { headers, statusCode: 500, body: error })
+        callback(null, { headers, statusCode: 500, body: JSON.stringify(error) })
       })
   }
 }
